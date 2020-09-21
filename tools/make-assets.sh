e9274d6e3050ffec600706c2513740540554b101
Ian Parker <ian@geekprojects.com>
2020-09-21 19:16:35 +0100

    Update tools to nuTensor and use nuAssets

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -13,19 +13,19 @@ cp ./assets/assets.json $DES/
 
 if [ -n "${TRAVIS_TAG}" ]; then
   pushd .. > /dev/null
-  git clone --depth 1 https://github.com/uBlockOrigin/uAssets.git
+  git clone --depth 1 https://github.com/geekprojects/nuAssets.git
   popd > /dev/null
 fi
 
 mkdir $DES/thirdparties
-cp -R ../uAssets/thirdparties/hosts-file.net             $DES/thirdparties/
-cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
-cp -R ../uAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
-cp -R ../uAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
-cp -R ../uAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
-cp -R ../uAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
-cp -R ../uAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/hosts-file.net             $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
+cp -R ../nuAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
 mkdir $DES/umatrix
-cp -R ../uAssets/recipes/*                               $DES/umatrix/
+cp -R ../nuAssets/recipes/*                               $DES/umatrix/
 
 echo "done."
--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -2,11 +2,11 @@
 #
 # This script assumes a linux environment
 
-echo "*** uMatrix.firefox: Creating web store package"
-echo "*** uMatrix.firefox: Copying files"
+echo "*** nuTensor.firefox: Creating web store package"
+echo "*** nuTensor.firefox: Copying files"
 
 BLDIR=dist/build
-DES="$BLDIR"/uMatrix.firefox
+DES="$BLDIR"/nuTensor.firefox
 rm -rf $DES
 mkdir -p $DES
 
@@ -20,20 +20,20 @@ cp    LICENSE.txt                           $DES/
 cp    platform/firefox/*.js                 $DES/js/
 cp    platform/firefox/manifest.json        $DES/
 
-echo "*** uMatrix.firefox: Generating meta..."
+echo "*** nuTensor.firefox: Generating meta..."
 python tools/make-firefox-meta.py           $DES/
 
 if [ "$1" = all ]; then
-    echo "*** uMatrix.firefox: Creating package..."
+    echo "*** nuTensor.firefox: Creating package..."
     pushd $DES > /dev/null
     zip ../$(basename $DES).xpi -qr *
     popd > /dev/null
 elif [ -n "$1" ]; then
-    echo "*** uMatrix.firefox: Creating versioned package..."
+    echo "*** nuTensor.firefox: Creating versioned package..."
     pushd $DES > /dev/null
     zip ../$(basename $DES).xpi -qr *
     popd > /dev/null
-    mv "$BLDIR"/uMatrix.firefox.xpi "$BLDIR"/uMatrix_"$1".firefox.xpi
+    mv "$BLDIR"/nuTensor.firefox.xpi "$BLDIR"/nuTensor"$1".firefox.xpi
 fi
 
-echo "*** uMatrix.firefox: Package done."
+echo "*** nuTensor.firefox: Package done."
